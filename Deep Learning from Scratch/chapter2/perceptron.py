1512fd638ea1c61480eac3a51f4470fbd7576cb4
chenbinbin <chenbinbin20@huawei.com>
2023-06-15 14:49:38 +0800

    chapter2

---
+++ Deep Learning from Scratch/chapter2/perceptron.py
@@ -0,0 +1,68 @@
+# 与门的实现
+def AND1(x1, x2):
+    w1, w2, theta = 0.5, 0.5, 0.7
+    tmp = x1 * w1 + x2 * w2
+    if tmp <= theta:
+        return 0
+    return 1
+
+
+print(AND1(0, 0))
+print(AND1(0, 1))
+print(AND1(1, 0))
+print(AND1(1, 1))
+
+import numpy as np
+
+x = np.array([0, 1])
+w = np.array([0.5, 0.5])
+b = -0.7
+print(x * w)
+print(np.sum(w * x))
+print(np.sum(x * w) + b)
+
+
+def AND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.7
+    tmp = np.sum(w * x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+
+def NAND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([-0.5, -0.5])
+    b = 0.7
+    tmp = np.sum(w * x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+
+def OR(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.2
+    tmp = np.sum(w * x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+
+def XOR(x1, x2):
+    s1 = NAND(x1, x2)
+    s2 = OR(x1, x2)
+    y = AND(x1, x2)
+    return y
+
+
+print(XOR(0, 0))
+print(XOR(1, 0))
+print(XOR(0, 1))
+print(XOR(1, 1))

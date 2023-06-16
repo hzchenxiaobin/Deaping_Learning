7c72888b75d80bcb46cad2989930e12c2c6dacb4
chenbinbin <chenbinbin20@huawei.com>
2023-06-16 11:27:31 +0800

    chapter3

--- Deep Learning from Scratch/ch03/neutralnet_mnist.py
+++ Deep Learning from Scratch/ch03/neutralnet_mnist.py
@@ -21,18 +21,20 @@ def init_network():
 
     return network
 
+
 def predict(network, x):
-    W1, W2, W3 = network['W1'], network['W2'], network['W3']
-    b1, b2, b3 = network['b1'], network['b2'], network['b3']
+    W1, W2, W3 = network["W1"], network["W2"], network["W3"]
+    b1, b2, b3 = network["b1"], network["b2"], network["b3"]
     a1 = np.dot(x, W1) + b1
     z1 = sigmoid(a1)
     a2 = np.dot(z1, W2) + b2
     z2 = sigmoid(a2)
     a3 = np.dot(z2, W3) + b3
     y = softmax(a3)
-    
+
     return y
-    
+
+
 x, t = get_data()
 network = init_network()
 
@@ -43,3 +45,18 @@ for i in range(len(x)):
     if p == t[i]:
         accuracy_cnt += 1
 print("Accuracy:" + str(float(accuracy_cnt) / len(x)))
+
+# 批处理
+x, t = get_data()
+network = init_network()
+batch_size = 100
+accuracy_cnt = 0
+
+for i in range(0, len(x), batch_size):
+    x_batch = x[i:i+batch_size]
+    y_batch = predict(network, x_batch)
+    p = np.argmax(y_batch, axis=1)
+    accuracy_cnt += np.sum(p == t[i:i+batch_size])
+
+print("Accuracy:" + str(float(accuracy_cnt) / len(x)))
+

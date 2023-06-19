ab1ddee576e0b914d53a79092fb3fbdeb820fac0
hzchenxiaobin <87942859+hzchenxiaobin@users.noreply.github.com>
2023-06-19 23:11:44 +0800

    ch04

--- Deep Learning from Scratch/ch04/mean_squared_error.py
+++ Deep Learning from Scratch/ch04/mean_squared_error.py
@@ -1,12 +1,14 @@
+import numpy as np
+
 def mean_squared_error(y, t):
     return 0.5 * np.sum((y - t) ** 2)
 
-# import numpy as np
-# t = [0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
-# y = [0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0]
-# v = mean_squared_error(np.array(y), np.array(t))
-# print(v)
+import numpy as np
+t = [0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
+y = [0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0]
+v = mean_squared_error(np.array(y), np.array(t))
+print(v)
 
-# y = [0.1, 0.05, 0.1, 0.0, 0.05, 0.1, 0.0, 0.6, 0.0, 0.0]
-# v = mean_squared_error(np.array(y), np.array(t))
-# print(v)
+y = [0.1, 0.05, 0.1, 0.0, 0.05, 0.1, 0.0, 0.6, 0.0, 0.0]
+v = mean_squared_error(np.array(y), np.array(t))
+print(v)
